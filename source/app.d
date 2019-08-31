8cef98a1298e59b2832d90908369074636026d34
Reavershark <jonasty1999@gmail.com>
2019-09-01 01:48:03 +0200

    Read memory

--- source/app.d
+++ source/app.d
@@ -1,41 +1,13 @@
-import std.conv;
 import std.stdio;
-import std.process;
-import std.string;
 import offsets;
-
-import core.sys.posix.sys.types;
-import core.sys.posix.sys.uio;
-import uio_ext;
-
-pid_t pidof(string name)
-{
-    auto pipes = pipeProcess(["pidof", name], Redirect.stdout);
-    scope(exit) wait(pipes.pid);
-
-    // Only return first match
-    pid_t pid = to!pid_t(pipes.stdout.readln.split("\n")[0].split(" ")[0]);
-    return pid;
-}
-
-bool Read(pid_t pid, void* address, void* buffer, size_t size) {
-	iovec[1] local;
-	iovec[1] remote;
-
-	local[0].iov_base = buffer;
-	local[0].iov_len = size;
-	remote[0].iov_base = address;
-	remote[0].iov_len = size;
-
-	return (process_vm_readv(pid, local.ptr, 1, remote.ptr, 1, 0) == size);
-}
-
+import memory;
 
 void main()
 {
     auto offsets = new Offsets("data/csgo.json");
+    auto address = 0x020d0000 + offsets.getSignature("dwClientState") + offsets.getSignature("dwClientState_Map");
 
     char[1024] buf;
-    Read(pidof("vim"), cast(char*) 0x7f9f9b2ee000, buf.ptr, 1024);
+    Read(pidof("csgo_linux64"), cast(char*) 0x7fb890ea0f10, buf.ptr, 1024);
     writeln(buf);
 }
---
+++ source/memory.d
@@ -0,0 +1,110 @@
+import std.conv;
+import std.process;
+import std.string;
+
+import core.sys.posix.sys.types;
+import core.sys.posix.sys.uio;
+import uio_ext;
+
+pid_t pidof(string name)
+{
+    auto pipes = pipeProcess(["pidof", name], Redirect.stdout);
+    scope(exit) wait(pipes.pid);
+
+    // Only return first match
+    pid_t pid = to!pid_t(pipes.stdout.readln.split("\n")[0].split(" ")[0]);
+    return pid;
+}
+
+bool Read(pid_t pid, void* address, void* buffer, size_t size) {
+    iovec[1] local;
+    iovec[1] remote;
+
+    local[0].iov_base = buffer;
+    local[0].iov_len = size;
+    remote[0].iov_base = address;
+    remote[0].iov_len = size;
+
+    return (process_vm_readv(pid, local.ptr, 1, remote.ptr, 1, 0) == size);
+}
+
+/*
+void ParseMaps() {
+    regions.clear();
+
+    std::ifstream maps("/proc/" + pidStr + "/maps");
+
+    std::string line;
+    while (std::getline(maps, line)) {
+        std::istringstream iss(line);
+        std::string memorySpace, permissions, offset, device, inode;
+        if (iss >> memorySpace >> permissions >> offset >> device >> inode) {
+            std::string pathname;
+
+            for (size_t ls = 0, i = 0; i < line.length(); i++) {
+                if (line.substr(i, 1).compare(" ") == 0) {
+                    ls++;
+
+                    if (ls == 5) {
+                        size_t begin = line.substr(i, line.size()).find_first_not_of(' ');
+
+                        if (begin != -1) {
+                            pathname = line.substr(begin + i, line.size());
+                        } else {
+                            pathname.clear();
+                        }
+                    }
+                }
+            }
+
+            MapModuleMemoryRegion region;
+
+            size_t memorySplit = memorySpace.find_first_of('-');
+            size_t deviceSplit = device.find_first_of(':');
+
+            std::stringstream ss;
+
+            if (memorySplit != -1) {
+                ss << std::hex << memorySpace.substr(0, memorySplit);
+                ss >> region.start;
+                ss.clear();
+                ss << std::hex << memorySpace.substr(memorySplit + 1, memorySpace.size());
+                ss >> region.end;
+                ss.clear();
+            }
+
+            if (deviceSplit != -1) {
+                ss << std::hex << device.substr(0, deviceSplit);
+                ss >> region.deviceMajor;
+                ss.clear();
+                ss << std::hex << device.substr(deviceSplit + 1, device.size());
+                ss >> region.deviceMinor;
+                ss.clear();
+            }
+
+            ss << std::hex << offset;
+            ss >> region.offset;
+            ss.clear();
+            ss << inode;
+            ss >> region.inodeFileNumber;
+
+            region.readable = (permissions[0] == 'r');
+            region.writable = (permissions[1] == 'w');
+            region.executable = (permissions[2] == 'x');
+            region.shared = (permissions[3] != '-');
+
+            if (!pathname.empty()) {
+                region.pathname = pathname;
+
+                size_t fileNameSplit = pathname.find_last_of('/');
+
+                if (fileNameSplit != -1) {
+                    region.filename = pathname.substr(fileNameSplit + 1, pathname.size());
+                }
+            }
+
+            regions.push_back(region);
+        }
+    }
+}
+*/
