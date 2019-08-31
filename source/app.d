7ebdbec41ce6ef107cf79accf26a97e50113da1b
Reavershark <jonasty1999@gmail.com>
2019-08-31 22:14:47 +0200

    pidof() function

--- source/app.d
+++ source/app.d
@@ -1,7 +1,21 @@
+import std.conv;
 import std.stdio;
+import std.process;
+import std.string;
 import offsets;
+
+ulong pidof(string name)
+{
+    auto pipes = pipeProcess(["pidof", name], Redirect.stdout);
+    scope(exit) wait(pipes.pid);
+
+    // Only return first match
+    ulong pid = to!long(pipes.stdout.readln.split("\n")[0].split(" ")[0]);
+    return pid;
+}
 
 void main()
 {
     auto offsets = new Offsets("data/csgo.json");
+    writeln(pidof("pidof"));
 }
