b3fcb94edb9d7ff3a68b855c6b94968cf6c3fd5e
Reavershark <jonasty1999@gmail.com>
2019-09-05 23:43:01 +0200

    Add app.d functionality back

--- source/app.d
+++ source/app.d
@@ -7,11 +7,9 @@ import memory;
 import draw;
 
 import std.conv : to;
-import std.concurrency : spawn;
 
 void main()
 {
-    /*
     int pid = pidof("csgo_linux64");
     Handle handle = new Handle(pid);
     Addresses addresses = new Addresses(pid);
@@ -20,26 +18,11 @@ void main()
     write("clientState: 0x");
     writefln!("%x")(clientState);
 
-    //Netvars.dump(handle, clientState);
+    Netvars.dump(handle, clientState);
 
     ulong localPlayerPtr = addresses.getLocalPlayerPtr(handle);
     write("localPlayer: 0x");
     writefln!("%x")(localPlayerPtr);
-
-    while (true)
-    {
-        ulong localPlayer = handle.read!ulong(localPlayerPtr);
-
-        // Recoil angle
-        ulong angleAddr = localPlayer + 0x3700 + 0x74;
-	    QAngle angle = handle.read!QAngle(angleAddr);
-
-        //handle.write(angleAddr, &angle, QAngle.sizeof);
-        writeln("Pitch/Yaw: ", angle.x, " ", angle.y);
-
-        Thread.sleep(dur!("msecs")(50));
-    }
-    */
 
     Draw draw = new Draw();
     new Thread({draw.init();}).start();
@@ -48,11 +31,16 @@ void main()
     {
         Thread.sleep(dur!("usecs")(1));
     }
-    int i;
     while(true)
     {
+        ulong localPlayer = handle.read!ulong(localPlayerPtr);
+        // Recoil angle
+        ulong angleAddr = localPlayer + 0x3700 + 0x74;
+	    QAngle angle = handle.read!QAngle(angleAddr);
+        //writeln("Pitch/Yaw: ", angle.x, " ", angle.y);
+
         draw.start();
-        draw.drawCrossHair(i++, 0);
+        //draw.drawCrossHair(to!int(angle.x), to!int(angle.y));
         draw.end();
         Thread.sleep(dur!("msecs")(1));
     }
